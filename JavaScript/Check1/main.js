5c619229542d8616eeb4166741d4d5b843152cff
salty815 <salty815@gmail.com>
2022-04-24 19:46:13 +0900

    check1提出

--- JavaScript/Check1/main.js
+++ JavaScript/Check1/main.js
@@ -1,14 +1,43 @@
 
 // 問1
 let numbers = [2, 5, 12, 13, 15, 18, 22];
+
 //ここに答えを実装してください。↓↓↓
-for (const numIndex = 0; numIndex < numbers.length; numIndex++) {
+let num = 0;
+for (let numIndex = 0; numIndex < numbers.length; numIndex++) {
     if (numbers[numIndex] % 2 === 0) {
-        let num = numbers[numIndex];
-        isEven();
+        num = numbers[numIndex];
+        isEven(num);
     }
 }
 
-function isEven() {
+function isEven(num) {
     console.log(num + 'は偶数です');
 }
+
+// 問2
+class Car {
+    constructor(gass, num) {
+        this.gass = gass;
+        this.num = num;
+    }
+
+    getNumGas() {
+        console.log("ガソリンは" + this.gass + "です。ナンバーは" + this.num + "です")
+    }
+}
+
+// let testCar = new Car("sekiyu", 1234);
+// testCar.getNumGas();
+
+
+
+
+
+
+
+
+
+
+
+
