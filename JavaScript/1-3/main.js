95e5017b27eefea8be7b4d0e25fb32e24fc37ce9
salty815 <salty815@gmail.com>
2022-04-30 11:45:46 +0900

    1-3 修正

--- JavaScript/1-3/main.js
+++ JavaScript/1-3/main.js
@@ -1,3 +1,5 @@
+
+// 問1
 let a = 100;
 if (a !== 10) {
     console.log(a);
@@ -6,4 +8,20 @@ if (a !== 10) {
 let b = 12;
 if (10 <= b && b < 20 && b % 2 === 0) {
     console.log(b);
+}
+
+// 問2
+let x = 10;
+if (10 <= x && x <= 20) {
+    console.log("成功です");
+} else if (20 < x) {
+    console.log("失敗です");
+}
+
+// 問3
+let y = 0;
+if (y % 2 === 0) {
+    console.log("偶数です");
+} else if (!y % 2 === 0) {
+    console.log("奇数です");
 }
